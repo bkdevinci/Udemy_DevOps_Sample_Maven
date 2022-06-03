37ad562b2ec6013e42766e19eef4a7eb1247cce2
bkdevinci <bkdevincilabs@gmail.com>
2022-06-02 17:04:20 -0700

    update maven project dsl from terminal

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -1,5 +1,5 @@
 job('First-Maven-Project-Via-DSL') {
-  description("First Maven job generated using DSL on ${new Date()}, the project is a small Maven project hosted on github.")
+  description("First Maven job generated with DSL on ${new Date()}, the project is a small Maven project hosted on github.")
   scm {
     git("https://github.com/bkdevinci/Udemy_DevOps_Sample_Maven.git", 'master')
   }
